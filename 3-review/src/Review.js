0f7eb578387d91be68f3f5da7d2c0da409b24be6
Saksham Jaiswal <sakshamjaiswal@six30labs.io>
2021-07-15 22:28:02 +0530

    adding new project

---
+++ 3-review/src/Review.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index,setIndex]=useState(0)
+  return <h2>review component</h2>;
+};
+
+export default Review;
